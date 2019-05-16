c19aa9b91cc111324a2958028484b286d6b1f3de
8957444432 <895744432@qq.com>
2019-05-16 13:19:25 +0800

    添加

---
+++ api/auth.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"net/http"
+	"video/api/session"
+)
+
+var HEADER_FIELD_SESSION = "X-Session-Id"//x为自定义head
+var HEADER_FIELD_UNAME = "X-User-Name"
+
+func validateUserSession(r *http.Request) bool {
+	sid:=r.Header.Get(HEADER_FIELD_SESSION)
+	if len(sid)==0{
+		return false
+	}
+
+	uname, ok := session.IsSessionExpired(sid)
+	if ok {
+		return false
+	}
+
+	//如果没有过期,那么就把用户名加入到HEADER_FIELD_UNAME里面
+	r.Header.Add(HEADER_FIELD_UNAME, uname)
+	return true
+}
+
+func ValidateUser(w http.ResponseWriter, r *http.Request) bool {
+	uname:=r.Header.Get(HEADER_FIELD_UNAME)
+	if len(uname)==0{
+
+		//sendErrorResponse(w, defs.ErrorNotAuthUser)
+		return false
+	}
+
+	return true
+}
